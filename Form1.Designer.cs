8e7dae55be615b015808e6155d1ef9f8f21b3db7
Turtle <officiallyturtlesquared@gmail.com>
2019-10-25 18:13:57 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace KeyboardCrypto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Type your text:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Result:";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(15, 39);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(825, 20);
+            this.inputTextBox.TabIndex = 2;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(15, 94);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(825, 20);
+            this.outputTextBox.TabIndex = 3;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.BackColor = System.Drawing.Color.White;
+            this.encryptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.encryptButton.Location = new System.Drawing.Point(681, 10);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 4;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = false;
+            this.encryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.BackColor = System.Drawing.Color.White;
+            this.decryptButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.decryptButton.Location = new System.Drawing.Point(762, 10);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 5;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = false;
+            this.decryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // applyButton
+            // 
+            this.applyButton.BackColor = System.Drawing.Color.White;
+            this.applyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.applyButton.Location = new System.Drawing.Point(762, 65);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(75, 23);
+            this.applyButton.TabIndex = 6;
+            this.applyButton.Text = "Process";
+            this.applyButton.UseVisualStyleBackColor = false;
+            this.applyButton.Click += new System.EventHandler(this.ApplyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(849, 123);
+            this.Controls.Add(this.applyButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Keyboard Crypto Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button applyButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyboardCrypto
+{
+    public partial class Form1 : Form
+    {
+        #region basic definitions and other things that need to be collapsed for this code to be legible
+        public bool isEncrypt;
+        public bool isDecrypt;
+        public void EncryptShifterFunction(string input)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString() == "1") { builder.Append("2"); }
+                if (input[i].ToString() == "2") { builder.Append("3"); }
+                if (input[i].ToString() == "3") { builder.Append("4"); }
+                if (input[i].ToString() == "4") { builder.Append("5"); }
+                if (input[i].ToString() == "5") { builder.Append("6"); }
+                if (input[i].ToString() == "6") { builder.Append("7"); }
+                if (input[i].ToString() == "7") { builder.Append("8"); }
+                if (input[i].ToString() == "8") { builder.Append("9"); }
+                if (input[i].ToString() == "9") { builder.Append("0"); }
+                if (input[i].ToString() == "0") { builder.Append("1"); }
+                if (input[i].ToString() == "q") { builder.Append("w"); }
+                if (input[i].ToString() == "w") { builder.Append("e"); }
+                if (input[i].ToString() == "e") { builder.Append("r"); }
+                if (input[i].ToString() == "r") { builder.Append("t"); }
+                if (input[i].ToString() == "t") { builder.Append("y"); }
+                if (input[i].ToString() == "y") { builder.Append("u"); }
+                if (input[i].ToString() == "u") { builder.Append("i"); }
+                if (input[i].ToString() == "i") { builder.Append("o"); }
+                if (input[i].ToString() == "o") { builder.Append("p"); }
+                if (input[i].ToString() == "p") { builder.Append("q"); }
+                if (input[i].ToString() == "a") { builder.Append("s"); }
+                if (input[i].ToString() == "s") { builder.Append("d"); }
+                if (input[i].ToString() == "d") { builder.Append("f"); }
+                if (input[i].ToString() == "f") { builder.Append("g"); }
+                if (input[i].ToString() == "g") { builder.Append("h"); }
+                if (input[i].ToString() == "h") { builder.Append("j"); }
+                if (input[i].ToString() == "j") { builder.Append("k"); }
+                if (input[i].ToString() == "k") { builder.Append("l"); }
+                if (input[i].ToString() == "l") { builder.Append("a"); }
+                if (input[i].ToString() == "z") { builder.Append("x"); }
+                if (input[i].ToString() == "x") { builder.Append("c"); }
+                if (input[i].ToString() == "c") { builder.Append("v"); }
+                if (input[i].ToString() == "v") { builder.Append("b"); }
+                if (input[i].ToString() == "b") { builder.Append("n"); }
+                if (input[i].ToString() == "n") { builder.Append("m"); }
+                if (input[i].ToString() == "m") { builder.Append("z"); }
+                if (input[i].ToString() == " ") { builder.Append(" "); }
+                else
+                {
+                    //donothing
+                }
+            }
+            outputTextBox.Text = builder.ToString();
+        }
+        public void DecryptShifterFunction(string input)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString() == "1") { builder.Append("0"); }
+                if (input[i].ToString() == "2") { builder.Append("1"); }
+                if (input[i].ToString() == "3") { builder.Append("2"); }
+                if (input[i].ToString() == "4") { builder.Append("3"); }
+                if (input[i].ToString() == "5") { builder.Append("4"); }
+                if (input[i].ToString() == "6") { builder.Append("5"); }
+                if (input[i].ToString() == "7") { builder.Append("6"); }
+                if (input[i].ToString() == "8") { builder.Append("7"); }
+                if (input[i].ToString() == "9") { builder.Append("8"); }
+                if (input[i].ToString() == "0") { builder.Append("9"); }
+                if (input[i].ToString() == "q") { builder.Append("p"); }
+                if (input[i].ToString() == "w") { builder.Append("q"); }
+                if (input[i].ToString() == "e") { builder.Append("w"); }
+                if (input[i].ToString() == "r") { builder.Append("e"); }
+                if (input[i].ToString() == "t") { builder.Append("r"); }
+                if (input[i].ToString() == "y") { builder.Append("t"); }
+                if (input[i].ToString() == "u") { builder.Append("y"); }
+                if (input[i].ToString() == "i") { builder.Append("u"); }
+                if (input[i].ToString() == "o") { builder.Append("i"); }
+                if (input[i].ToString() == "p") { builder.Append("o"); }
+                if (input[i].ToString() == "a") { builder.Append("l"); }
+                if (input[i].ToString() == "s") { builder.Append("a"); }
+                if (input[i].ToString() == "d") { builder.Append("s"); }
+                if (input[i].ToString() == "f") { builder.Append("d"); }
+                if (input[i].ToString() == "g") { builder.Append("f"); }
+                if (input[i].ToString() == "h") { builder.Append("g"); }
+                if (input[i].ToString() == "j") { builder.Append("h"); }
+                if (input[i].ToString() == "k") { builder.Append("j"); }
+                if (input[i].ToString() == "l") { builder.Append("k"); }
+                if (input[i].ToString() == "z") { builder.Append("m"); }
+                if (input[i].ToString() == "x") { builder.Append("z"); }
+                if (input[i].ToString() == "c") { builder.Append("x"); }
+                if (input[i].ToString() == "v") { builder.Append("c"); }
+                if (input[i].ToString() == "b") { builder.Append("v"); }
+                if (input[i].ToString() == "n") { builder.Append("b"); }
+                if (input[i].ToString() == "m") { builder.Append("n"); }
+                if (input[i].ToString() == " ") { builder.Append(" "); }
+                else
+                { 
+                    //donothing
+                }
+            }
+            outputTextBox.Text = builder.ToString();
+        }
+
+
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            isEncrypt = true;
+            isDecrypt = false;
+            encryptButton.BackColor = Color.Blue;
+            encryptButton.ForeColor = Color.White;
+            decryptButton.BackColor = Color.White;
+            decryptButton.ForeColor = Color.Black;
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            isEncrypt = true;
+            isDecrypt = false;
+            encryptButton.BackColor = Color.Blue;
+            encryptButton.ForeColor = Color.White;
+            decryptButton.BackColor = Color.White;
+            decryptButton.ForeColor = Color.Black;
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            isEncrypt = false;
+            isDecrypt = true;
+            decryptButton.BackColor = Color.Blue;
+            decryptButton.ForeColor = Color.White;
+            encryptButton.BackColor = Color.White;
+            encryptButton.ForeColor = Color.Black;
+        }
+
+        private void ApplyButton_Click(object sender, EventArgs e)
+        {
+            if (isEncrypt == true)
+            {
+                EncryptShifterFunction(inputTextBox.Text);
+            }
+            else
+            {
+                DecryptShifterFunction(inputTextBox.Text);
+            }
+        }
+    }
+}
